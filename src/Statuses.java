20ccb994e842281e39328208c6a6bc2df4f74e72
Alex Costa <alex.callum.c@gmail.com>
2017-12-11 12:57:58 +0000

    chore: add status enum

---
+++ src/Statuses.java
@@ -0,0 +1,15 @@
+
+/**
+ * Enumeration class Statuses - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Statuses
+{
+    SignedOff,
+    LoggedIn,
+    Unheld,
+    Held,
+    Reserved
+}
