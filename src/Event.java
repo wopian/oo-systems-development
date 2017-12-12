ac0671e389df13c6b2ea1088020e44453a00eb71
Alexandre Costa <alex.callum.c@gmail.com>
2017-12-12 14:38:01 +0000

    chore: add event constructor and show method
    Still need to figure out how to assign the seats to the show for the assignment of promotions...

--- src/Event.java
+++ src/Event.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 /**
  * Class that describes what an event is and what it does
  *
@@ -8,14 +10,38 @@ import java.util.ArrayList;
 public class Event
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private ArrayList<Show> shows;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String eventName;
+    private int eventID;
+    private int status;
 
     /**
      * Constructor for objects of class Event
+     * @param newName The name for the new event
+     * @param start The start date for the new event
+     * @param end The end date for the new event
+     * @param lastEventID The ID of the last event in order to create to add the new event
      */
-    public Event()
+    public Event(String newName, LocalDate start, LocalDate end, int lastEventID)
     {
-        // initialise instance variables
-        x = 0;
+        eventName = newName;
+        startDate = start;
+        endDate = end;
+        eventID = (lastEventID + 1);
+        status = statuses.Confirmed;
+    }
+    
+    /**
+    * Method that creates a new show for this event
+    * @param start The start time and date of the show
+    * @param end The end time and date of the show
+    * @param MSPC The max seats per customer value for the show
+    */
+    // TODO: figure out how to add the seats to the show
+    public void addShow(LocalDateTime start, LocalDateTime end, int MSPC)
+    {
+      shows.add(new show(start, end, MSPC, shows.size()));
     }
 }
