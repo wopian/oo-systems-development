3e29a90e676fcc229aa7b3c2b11d1dabe33b17f7
Alex Costa <alex.callum.c@gmail.com>
2017-12-13 00:55:56 +0000

    feat: implement the Seat class

---
+++ src/Seat.java
@@ -0,0 +1,62 @@
+
+/**
+ * Class storing the data and behaviour of a seat in the
+ * Online Ticket Sale system
+ *
+ * @author Alex Costa
+ * @version 1.00
+ */
+public class Seat
+{
+    // instance variables
+    private int seatNumber;
+    private Statuses status;
+    private int promotionID;
+
+    /**
+     * Constructor for objects of class Seat
+     * @param number The number of the seat (should correspond with its index in the
+     * show's seats ArrayList)
+     */
+    public Seat(int number)
+    {
+        seatNumber = number;
+        status = Statuses.Unheld;
+    }
+    
+    /**
+     * A method that allows other methods to retrieve the number of the seat
+     * @return The number of the seat
+     */
+    public int getNumber()
+    {
+        return seatNumber;
+    }
+    
+    /**
+     * Method that allows other classes to retrieve the status of the seat
+     * @return The status of the seat (held, unheld, reserved)
+     */
+    public Statuses getStatus()
+    {
+        return status;
+    }
+    
+    /**
+     * Method that allows to change the status of a seat
+     * @param newStatus Contains the new status for the seat
+     */
+    public void setStatus(Statuses newStatus)
+    {
+        status = newStatus;
+    }
+    
+    /**
+     * Method that allows to assign a promotion to a seat
+     * @param promoID The ID of the promotion being assigned to the seat
+     */
+    public void setPromotion(int promoID)
+    {
+        promotionID = promoID;
+    }
+}
