2c9a5b5bab6e2e1b794c893d806ac0fc7193b69d
Alex Costa <alex.callum.c@gmail.com>
2017-12-13 00:56:10 +0000

    chore: implement the WeekDay enum

---
+++ src/WeekDay.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class WeekDay - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum WeekDay
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
